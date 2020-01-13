76228dc77cf23576ef491e7d140b4bcf19792837
zengxuw <zengxuw@foxmail.com>
2020-01-13 19:55:34 +0800

    harib05c

--- my practise/zxwos/bootpack.c
+++ my practise/zxwos/bootpack.c
@@ -3,13 +3,14 @@
 * @Author: zxw
 * @Date:   2020-01-10 19:21:10
 * @Last Modified by:   zxw
-* @Last Modified time: 2020-01-13 19:15:54
+* @Last Modified time: 2020-01-13 19:52:25
 */
 #include "bootpack.h"					 
 #include <stdio.h>
 
 struct MOUSE_DEC {
 	unsigned char buf[3], phase;
+	int x, y, btn;
 };
 
 extern struct FIFO8 keyfifo, mousefifo;  /*设立缓冲区*/
@@ -69,10 +70,19 @@ void HariMain(void)
 				i = fifo8_get(&mousefifo);
 				io_sti();
 				if (mouse_decode(&mdec, i) != 0) {
-					/*3字节凑齐了，把字节显示出来*/
-					sprintf(s,"%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 -1, 31);
-					putfonts8_asc(binfo->vram, binfo->scrnx, 32 , 16, COL8_FFFFFF, s);
+					/*数据的三个字节都齐了，把他们显示出来*/
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 15 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
 				}
 			}
 		}
@@ -130,8 +140,12 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
 	}
 	if (mdec->phase == 1) {
 		/*等待第一个字节*/
-		mdec->buf[0] = dat;
-		mdec->phase = 2;
+		if ((dat & 0xc8) == 0x08) { 
+			/*加上这个条件判断的原因是避免鼠标的偶尔失灵导致的数据丢失*/
+			/*如果第一字节正确*/
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+		}
 		return 0;
 	}
 	if (mdec->phase == 2) {
@@ -141,8 +155,19 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
 		return 0;
 	}
 	if (mdec->phase == 3) {
+		/*等待第三个字节到达，并在到达后做出处理*/
 		mdec->buf[2] = dat;
 		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0]) & 0x20 != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y; /*鼠标的y方向与画面符号相反*/
 		return 1;
 	}
 
